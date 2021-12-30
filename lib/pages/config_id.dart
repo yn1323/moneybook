b9d8889a18f0f1a426781b2a7b279c09a56daac0
yn1323 <yn1323@gmail.com>
2021-12-30 12:37:59 +0900

    feat: config layout

--- lib/pages/config_id.dart
+++ lib/pages/config_id.dart
@@ -11,12 +11,71 @@ class ConfigId extends ConsumerStatefulWidget {
 
 class _ConfigId extends ConsumerState<ConfigId> {
   final String title = '共有ID設定';
+  final _key = GlobalKey<FormState>();
+  final TextEditingController controller = TextEditingController();
+  final FocusNode focusNode = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
         backgroundColor: Colors.orange,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'IDを揃えることで他の人と家計簿を共有できます。',
+              style: TextStyle(fontSize: 14),
+            ),
+            Form(
+              key: _key,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextFormField(
+                    focusNode: focusNode,
+                    controller: controller,
+                    decoration: const InputDecoration(labelText: 'ID'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'IDを入力してください。';
+                      }
+                      return null;
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            controller.text = 'initial Id';
+                          },
+                          style: ElevatedButton.styleFrom(
+                              primary: Theme.of(context).colorScheme.secondary),
+                          child: const Text('初期IDに戻す'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            if (_key.currentState!.validate()) {
+                              print(controller.text);
+                            }
+                          },
+                          child: const Text('変更'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
