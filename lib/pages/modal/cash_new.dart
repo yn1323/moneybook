73b89fba39e62fb782c2c5aaab92e72805e2a1a1
yn1323 <yn1323@gmail.com>
2022-01-10 13:15:21 +0900

    fix: separate form files

--- lib/pages/modal/cash_new.dart
+++ lib/pages/modal/cash_new.dart
@@ -1,7 +1,7 @@
 import 'package:moneybook/imports.dart';
-import 'package:moneybook/providers/member.dart';
-import 'package:intl/intl.dart';
-import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:moneybook/widgets/form/category_selector.dart';
+import 'package:moneybook/widgets/form/date_picker.dart';
+import 'package:moneybook/widgets/form/member_selecter.dart';
 
 class CashNew extends ConsumerStatefulWidget {
   const CashNew({
@@ -16,26 +16,15 @@ class _CashNew extends ConsumerState<CashNew> {
   final title = '';
   final _key = GlobalKey<FormState>();
   final TextEditingController dateController = TextEditingController();
+  final TextEditingController memberController =
+      TextEditingController(text: '');
+  final TextEditingController categoryController =
+      TextEditingController(text: '');
   final FocusNode focusNode = FocusNode();
-  DateTime date = DateTime.now();
-  DateFormat formatter = DateFormat('yyyy-MM-dd');
 
   @override
   void initState() {
     super.initState();
-    dateController.text = formatter.format(date);
-  }
-
-  Future<void> selectDate(BuildContext context) async {
-    final DateTime? picked = await showDatePicker(
-        context: context,
-        initialDate: date,
-        firstDate: DateTime(1990),
-        lastDate: DateTime.now().add(const Duration(days: 360)));
-    if (picked != null) {
-      setState(() => date = picked);
-      dateController.text = formatter.format(date);
-    }
   }
 
   @override
@@ -54,25 +43,10 @@ class _CashNew extends ConsumerState<CashNew> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  GestureDetector(
-                    onTap: () => selectDate(context),
-                    child: AbsorbPointer(
-                      child: TextFormField(
-                        focusNode: focusNode,
-                        controller: dateController,
-                        decoration: const InputDecoration(
-                          labelText: '日付',
-                          prefixIcon: Icon(Icons.today),
-                        ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return '日付を入力してください。';
-                          }
-                          return null;
-                        },
-                      ),
-                    ),
-                  ),
+                  DatePicker(
+                      controller: dateController, initialDate: DateTime.now()),
+                  MemberSelecter(controller: memberController),
+                  CategorySelecter(controller: categoryController),
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Row(
@@ -80,10 +54,14 @@ class _CashNew extends ConsumerState<CashNew> {
                       children: [
                         ElevatedButton(
                           onPressed: () {
+                            print(dateController.text);
+                            print(memberController.text);
+                            print(categoryController.text);
                             if (_key.currentState!.validate()) {
                               FocusScope.of(context).unfocus();
+
                               // add(controller.text);
-                              Navigator.of(context).pop();
+                              // Navigator.of(context).pop();
                             }
                           },
                           child: const Text('新規追加'),
@@ -94,36 +72,6 @@ class _CashNew extends ConsumerState<CashNew> {
                 ],
               ),
             ),
-            ElevatedButton(
-                onPressed: () async {
-                  var result = await showModalBottomSheet(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          ListTile(
-                            leading: Icon(Icons.music_note),
-                            title: Text('Music'),
-                            onTap: () => Navigator.of(context).pop(1),
-                          ),
-                          ListTile(
-                            leading: Icon(Icons.videocam),
-                            title: Text('Video'),
-                            onTap: () => Navigator.of(context).pop(2),
-                          ),
-                          ListTile(
-                            leading: Icon(Icons.camera),
-                            title: Text('Picture'),
-                            onTap: () => Navigator.of(context).pop(3),
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                  print('bottom sheet result: $result');
-                },
-                child: const Text('aaa'))
           ],
         ),
       ),
---
+++ lib/widgets/form/member_selecter.dart
@@ -0,0 +1,58 @@
+import 'package:moneybook/imports.dart';
+import 'package:moneybook/providers/member.dart';
+
+class MemberSelecter extends ConsumerStatefulWidget {
+  const MemberSelecter({
+    Key? key,
+    required this.controller,
+  }) : super(key: key);
+
+  final TextEditingController controller;
+  @override
+  _MemberSelecter createState() => _MemberSelecter();
+}
+
+class _MemberSelecter extends ConsumerState<MemberSelecter> {
+  @override
+  Widget build(BuildContext context) {
+    final members = ref.read(memberProvider).getMember();
+    return GestureDetector(
+      onTap: () async {
+        String? result = await showModalBottomSheet(
+          context: context,
+          builder: (BuildContext context) {
+            return Column(
+              mainAxisSize: MainAxisSize.min,
+              children: members
+                  .map(
+                    (member) => ListTile(
+                      title: Text(member),
+                      onTap: () => Navigator.of(context).pop(member),
+                    ),
+                  )
+                  .toList(),
+            );
+          },
+        );
+        if (result != null) {
+          widget.controller.text = result;
+        }
+      },
+      child: AbsorbPointer(
+        child: TextFormField(
+          controller: widget.controller,
+          decoration: const InputDecoration(
+            labelText: '支払',
+            prefixIcon: Icon(Icons.person),
+          ),
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return '支払い者を入力してください。';
+            }
+            return null;
+          },
+        ),
+      ),
+    );
+  }
+}
